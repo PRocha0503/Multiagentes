cc48c1b651fc4ebf94b8f2d462cf59909b42c5ec
Luisja <52958375+Luisjakg@users.noreply.github.com>
2022-11-24 14:09:49 -0600

    Ovni update 1.0

---
+++ Simulacion Multiagentes/Assets/Scripts/Visuals/Vibration.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Vibration : MonoBehaviour
+{
+    readonly float speed = 5.0f;
+    readonly float intensity = 0.1f;
+    private float randomSpeed;
+        
+    // Random Speed
+
+    private void Start()
+    {
+        randomSpeed = Random.Range(speed - 1f, speed);
+    }
+
+    void Update()
+    {
+        
+ 
+        transform.localPosition = intensity * new Vector3(
+            Mathf.PerlinNoise(randomSpeed * Time.time, 1),
+            2.459739f,
+            Mathf.PerlinNoise(randomSpeed * Time.time, 3));
+    }
+}
